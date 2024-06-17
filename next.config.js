947edb2cafe73728bfc3505dfeedcfa50959c88b
Gabriel442018 <37821374+Gabriel4420@users.noreply.github.com>
2024-06-17 11:16:27 -0300

    feat: :wrench: arquivos de configuração

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          },{
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },{
+            key: 'Access-Control-Allow-Origin',
+            value: '*'
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
